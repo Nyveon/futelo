f06261b106fb5403fc43bdba500b89c2d477d9c4
dabaez <mastil.daniel@gmail.com>
2025-02-22 17:26:16 -0300

    mo cors mo problems

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from private_info import FRONTEND_URL
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware, allow_origins=[FRONTEND_URL])
+app.add_middleware(CORSMiddleware, allow_origins=[FRONTEND_URL], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/stats")
 async def stats(user_id:int):
