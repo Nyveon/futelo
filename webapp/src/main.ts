507d130b12335d08d76a55b254965edfb60e19e6
Eric K <54124297+Nyveon@users.noreply.github.com>
2025-02-27 10:33:22 -0300

    chore: moving everything to webapp folder

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,17 +1,18 @@
-import Alpine from 'alpinejs'
-import './css/style.css'
- 
-window.Alpine = Alpine
-Alpine.start()
+import "./css/style.css";
+import "./css/normalize.css";
+import Alpine from "alpinejs";
 
-const userId = "1" 
+window.Alpine = Alpine;
+Alpine.start();
 
-const response = await fetch('http://localhost:8000/stats?user_id=' + userId,{
-    method: 'GET',
-    headers: {
-        'ngrok-skip-browser-warning': 'truuu',
-    },
-})
+// const userId = "1";
 
-const data = await response.json()
-console.log(data)
+// const response = await fetch("http://localhost:8000/stats?user_id=" + userId, {
+// 	method: "GET",
+// 	headers: {
+// 		"ngrok-skip-browser-warning": "truuu",
+// 	},
+// });
+
+// const data = await response.json();
+// console.log(data);
