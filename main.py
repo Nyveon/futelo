fe5c24076ca00f8da837938998531eef16b5c000
AngrySeal <mastil.daniel@gmail.com>
2025-05-25 22:18:00 -0400

    small changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
 from api_routes import router as api_router  # Import the router from api_routes.py
 
 # Create the FastAPI app instance
